ce3add1ac174fae0c5e80b292fc9b288c3ddb63a
Michael Krause <michael.krause@ucsc.edu>
2019-02-07 16:50:34 -0800

    Get ticket information from Zenhub via API (TLP-837)
    This is part of an epic to syncronize between ticket management
    systems in use, such as ZenHub and Jira.
    
    * returns information of a GitHub ticket by hitting the ZenHub API
    * output is a dictionary with keys
        * story number
        * repository name
        * pipeline
        * story points
        * timestamp (of last change)
    * tests cover user input of non-existing
        * repo name
        * story number

---
+++ settings.py
@@ -0,0 +1,7 @@
+repo = dict(
+    AZUL = 139095537
+)
+
+giturl = dict(
+    URL = 'https://api.zenhub.io/p1/repositories/'
+)
---
+++ src/zenhub.py
@@ -0,0 +1,80 @@
+#!/usr/env/python3
+
+
+import os, sys, logging, requests, json
+sys.path.append(".")
+from settings import repo, giturl
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT)
+
+def main():
+    path_to_token = sys.argv[1]
+    repo_name = sys.argv[2]
+    issue = sys.argv[3]
+
+    zen = ZenHub(path_to_token=path_to_token,
+                 repo_name=repo_name,
+                 issue=issue)
+    print(json.dumps(zen.get_info()))
+
+
+class ZenHub():
+
+    def __init__(self, path_to_token=None, repo_name=None, issue=None):
+        self.token = self._get_token(path_to_token)
+        self.repo_name = repo_name
+        self.repo_id = self._get_repo_id(repo_name)
+        self.issue = str(issue)
+        self.url = self._generate_url()
+
+
+    def get_info(self):
+        url = self._generate_url()
+        logger.info(f'Getting pipeline, storypoints and timestamp for story {self.issue} in repo {self.repo_name}')
+        headers = {'X-Authentication-Token': self.token}
+        response = requests.get(url, headers=headers, verify=False)
+        if response.status_code == 200:
+            data = response.json()
+            pipeline = data['pipeline']['name']
+            storypoints = data['estimate']['value']
+            if data['plus_ones'] == []:
+                timestamp = 'Not available'
+            else:
+                timestamp = data['plus_ones']['created_at']
+            return {'Story number': self.issue,
+                    'Repository': self.repo_name,
+                    'Pipeline': pipeline,
+                    'Storypoints': storypoints,
+                    'Timestamp': timestamp}
+
+        else:
+            return response.json()
+
+    def _generate_url(self):
+        _url = giturl['URL']
+        return os.path.join(_url, self.repo_id, 'issues', self.issue)
+
+    @staticmethod
+    def _get_token(path_to_token):
+        try:
+            with open(path_to_token, 'r') as fh:
+                tok = fh.readlines()
+                return tok[0]
+        except FileNotFoundError as e:
+            logger.info(e.strerror)
+
+    @staticmethod
+    def _get_repo_id(repo_name):
+        try:
+            if repo_name == 'azul':
+                return str(repo['AZUL'])
+        except ValueError as err:
+            logger.info(f'{repo_name} is not a known repo')
+
+
+if __name__ == '__main__':
+    main()
