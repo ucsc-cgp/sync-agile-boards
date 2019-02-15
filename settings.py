a7ff73c9ded74528dc4c01dd5880125da55c4ce4
Michael Krause <michael.krause@ucsc.edu>
2019-02-15 10:01:56 -0800

    Prototyping basic auth

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
 repo = dict(
-    AZUL = 139095537
-)
+    AZUL=139095537
+    )
 
 giturl = dict(
-    URL = 'https://api.zenhub.io/p1/repositories/'
-)
+    URL='https://api.zenhub.io/p1/repositories/'
+    )
+
+auth = dict(
+    api_token_jira='~/.jira_config',
+    api_token_zenhub='~/.zenhub_config'
+    )
---
+++ src/jira.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from jira import JIRA
+
+
+class AccessJira:
+    """Return basic auth token."""
+
+    def get_access(username, base_url):
+        pass
+
